ffac1a088a7a6c8ab09a7fe10ae60f868c9fbfec
Amadewe <dewevre.amandine@gmail.com>
2019-03-04 15:26:18 +0100

    Ajout des exercices 1 et 2

---
+++ exercice 1/assets/js/canvas.js
@@ -0,0 +1,58 @@
+// getElementById() qui va permettre d'aller chercher et cibler l'élément <canvas> identifié par son attribut id unique (içi house)
+var c = document.getElementById('house');
+// la méthode getContext() de l'élément ainsi récupéré pour savoir dans quel contexte de dessin (2D ou 3D) le script va pouvoir agir,
+// et de quelles fonctions il pourra disposer.
+// Le contexte sera l'élément central de gestion de Canvas.
+var ctx = c.getContext('2d');
+
+// maison avec toit
+ctx.beginPath(); // Début du chemin
+ctx.moveTo(200,50); //choisir un point de départ (içi le haut du toit), (horizontal, vertical) les parenthèses sont des "paramètres"
+// lineTo permet de tracer un trait et prend en paramètre le point d'arrivée
+ctx.lineTo(300,150); //coin suppérieur droite de ma maison
+ctx.lineTo(300,300); //coin inférieur droite de ma maison
+ctx.lineTo(100,300); //coin inférieur gauche
+ctx.lineTo(100,150); //coin suppérieur gauche
+// closePath permet de finir le trac et revient au point d'origine.
+ctx.closePath(); // Fermeture du chemin
+ctx.fillStyle = '#a1d8e6'; // Définition de la couleur de remplissage
+ctx.fill();  // Remplissage du dernier chemin tracé
+
+//toit
+ctx.beginPath();
+ctx.moveTo(200,50);
+ctx.lineTo(300,150);
+ctx.lineTo(100,150);
+ctx.closePath();
+ctx.fillStyle = '#d7823e';
+ctx.fill();
+
+// fenêtre de gauche
+ctx.beginPath();
+ctx.moveTo(120,160);
+ctx.lineTo(170,160);
+ctx.lineTo(170,200);
+ctx.lineTo(120,200);
+ctx.closePath();
+ctx.fillStyle = '#c9c9c9';
+ctx.fill();
+
+// fenêtre de droite
+ctx.beginPath();
+ctx.moveTo(230,160);
+ctx.lineTo(280,160);
+ctx.lineTo(280,200);
+ctx.lineTo(230,200);
+ctx.closePath();
+ctx.fillStyle = '#c9c9c9';
+ctx.fill();
+
+// porte
+ctx.beginPath();
+ctx.moveTo(170,230);
+ctx.lineTo(230,230);
+ctx.lineTo(230,300);
+ctx.lineTo(170,300);
+ctx.closePath();
+ctx.fillStyle = '#c9c9c9';
+ctx.fill();
---
+++ exercice 2/assets/js/canvas.js
@@ -0,0 +1,28 @@
+// getElementById() qui va permettre d'aller chercher et cibler l'élément <canvas> identifié par son attribut id unique (içi house)
+var c = document.getElementById('house');
+// la méthode getContext() de l'élément ainsi récupéré pour savoir dans quel contexte de dessin (2D ou 3D) le script va pouvoir agir,
+// et de quelles fonctions il pourra disposer.
+// Le contexte sera l'élément central de gestion de Canvas.
+var ctx = c.getContext("2d");
+
+// toit
+ctx.fillStyle = "#d7823e";
+ctx.rotate(45 * Math.PI / 180);
+ctx.fillRect(150, -130, 140, 140);
+
+ctx.fillStyle = "#a1d8e6";
+//  fillRect(startx, starty, hauteur, largeur)
+ctx.rotate(-45 * Math.PI / 180);
+ctx.fillRect(100, 115, 200, 200);
+
+//carré fenêtre gauche
+ctx.fillStyle = "#c9c9c9";
+ctx.fillRect(120, 130, 60, 60);
+
+//carré fenêtre droite
+ctx.fillStyle = "#c9c9c9";
+ctx.fillRect(220, 130, 60, 60);
+
+//porte
+ctx.fillStyle = "#c9c9c9";
+ctx.fillRect(170, 215, 60, 100);
